dccee9d42a0b56dbbb08d4b2c727c4b86bf0dbf1
niyiseyi85 <110688772+niyiseyi85@users.noreply.github.com>
2024-09-15 19:20:59 +0100

    Configured the main entry point for the console application - Set up dependency injection for `ICalculatorService`, `IParser`, and `IValidator` - Implemented basic user input handling and processing logic

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StringCalculator.Entities;
+using StringCalculator.Service;
+using StringCalculator.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = ConfigureServices();
+
+            // Get the Calculator service
+            var calculatorService = serviceProvider.GetService<ICalculatorService>();
+
+            Console.WriteLine("Welcome to the Calculator. Press Ctrl+C to exit.");
+
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter input:");
+                    string input = Console.ReadLine();
+
+                    var result = calculatorService.Evaluate(input, OperationType.Add);
+                    Console.WriteLine($"Result: {result.result}");
+                    Console.WriteLine($"Formula: {result.formula}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IParser, Parser>();
+            services.AddTransient<IValidator, Validator>();
+            services.AddTransient<ICalculatorService, CalculatorService>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}   
+ 
