6f350d0390e103fb6d2459e44eb1029748eb47ed
niyiseyi85 <110688772+niyiseyi85@users.noreply.github.com>
2024-09-15 19:15:53 +0100

    - Implemented validation to throw exceptions for negative numbers, including a detailed error message listing all negative values encountered - Added support to ignore numbers greater than the configured upper bound (default 1000) by treating them as zero

---
+++ StringCalculator/Shared/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Shared
+{
+    public interface IValidator
+    {
+        void ValidateNumbers(List<int> numbers);
+    }
+    public class Validator : IValidator
+    {
+        private readonly bool _denyNegatives;
+        public void ValidateNumbers(List<int> numbers)
+        {
+            if (_denyNegatives)
+            {
+                var negativeNumbers = numbers.Where(n => n < 0).ToList();
+                if (negativeNumbers.Any())
+                {
+                    throw new ArgumentException($"Negatives not allowed: {string.Join(",", negativeNumbers)}");
+                }
+            }
+        }
+    }
+}
